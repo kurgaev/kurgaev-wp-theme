8bb422c9f0487de32533bd31ca69b4c5fae044ba
Denton John <kurgaev101@gmail.com>
2017-03-04 03:48:22 +0500

    blank template added, started making index page on flex

---
+++ header-blank.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package kurgaev
+ */
+
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
---
+++ page-blank.php
@@ -0,0 +1,9 @@
+<?php
+/*******************
+Template Name: blank
+********************/
+
+get_header( 'blank' );
+the_post();
+the_content();
+get_footer( 'blank' );
